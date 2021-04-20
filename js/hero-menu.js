029c1150123b392943f2e7bf3136b4921acc8a40
Azer <azer@MacBook-Air-Azer.local>
2021-04-20 15:17:34 +0400

    hw
    hw

---
+++ js/hero-menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('.hero__header_button_1').click(function(Event) {
+    $('.hero__dropdown_list_1').toggleClass('hero__dropdown_list_active')
+  })
+  $('.hero__header_button_2').click(function(Event) {
+    $('.hero__dropdown_list_2').toggleClass('hero__dropdown_list_active')
+  })
+  $('.hero__header_button_3').click(function(Event) {
+    $('.hero__dropdown_list_3').toggleClass('hero__dropdown_list_active')
+  })
+  $('.hero__header_button_4').click(function(Event) {
+    $('.hero__dropdown_list_4').toggleClass('hero__dropdown_list_active')
+  })
+  $('.hero__header_button_5').click(function(Event) {
+    $('.hero__dropdown_list_5').toggleClass('hero__dropdown_list_active')
+  })
+})
+
