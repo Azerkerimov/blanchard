b51dc2d49d2e694ac2112f93859364a36a65a951
Azer <azer@MacBook-Air-Azer.local>
2021-04-21 19:03:03 +0400

    ddd
    ddd

--- js/hero-menu.js
+++ js/hero-menu.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  $('.hero__header_button_1').click(function(Event) {
-    $('.hero__dropdown_list_1').toggleClass('hero__dropdown_list_active')
+  $('.hero-header-item__button--1').click(function(Event) {
+    $('.hero-dropdown--1').toggleClass('hero__dropdown_list_active')
   })
-  $('.hero__header_button_2').click(function(Event) {
-    $('.hero__dropdown_list_2').toggleClass('hero__dropdown_list_active')
+  $('.hero-header-item__button--2').click(function(Event) {
+    $('.hero-dropdown--2').toggleClass('hero__dropdown_list_active')
   })
-  $('.hero__header_button_3').click(function(Event) {
-    $('.hero__dropdown_list_3').toggleClass('hero__dropdown_list_active')
+  $('.hero-header-item__button--3').click(function(Event) {
+    $('.hero-dropdown--3').toggleClass('hero__dropdown_list_active')
   })
-  $('.hero__header_button_4').click(function(Event) {
-    $('.hero__dropdown_list_4').toggleClass('hero__dropdown_list_active')
+  $('.hero-header-item__button--4').click(function(Event) {
+    $('.hero-dropdown--4').toggleClass('hero__dropdown_list_active')
   })
-  $('.hero__header_button_5').click(function(Event) {
-    $('.hero__dropdown_list_5').toggleClass('hero__dropdown_list_active')
+  $('.hero-header-item__button--5').click(function(Event) {
+    $('.hero-dropdown--5').toggleClass('hero__dropdown_list_active')
   })
 })
 
